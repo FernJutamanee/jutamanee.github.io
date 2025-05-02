dcbe6ce7010018ee8d9441ed1ddb5cb71b6f83c1
FernJutamanee <jutamanee.thongbor@gmail.com>
2025-05-02 17:05:54 +0700

    Create project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>ยินดีต้อนรับสู่เว็บไซต์ของฉัน</h1>
+    <p>สร้างด้วย Vue 3 + TypeScript</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
