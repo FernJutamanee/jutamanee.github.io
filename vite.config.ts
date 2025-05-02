eebb2cf87f1947ebe7b0f240bd329ed97d030b6e
FernJutamanee <jutamanee.thongbor@gmail.com>
2025-05-02 17:30:24 +0700

    Change name repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-vue-site/", // ชื่อ repo ของคุณ
+  base: "/jutamanee.github.io/", // ชื่อ repo ของคุณ
   plugins: [vue()],
 });
